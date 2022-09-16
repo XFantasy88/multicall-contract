ee2ee8a00ba29d7f2bc51942ad1207d201fa2599
XFantasy88 <erencote@gmail.com>
2022-09-16 10:12:44 -0400

    multicall contract added

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,13 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+
+  networks: {
+    dogetest: {
+      url: 'https://rpc-testnet.dogechain.dog/',
+      chainId: 568,
+      accounts: [],
+      timeout: 600000000
+    }
+  }
 };
